fd5210d28afe836c8c48afd35ac4c9dab451c3c6
jefang15 <jeffreyfang@msn.com>
2022-03-20 17:04:20 -0400

    modified

--- PyCharm Notes.py
+++ PyCharm Notes.py
@@ -7,8 +7,6 @@
 # """ Block text """
 # Comment out text: highlight code or text > Command + /
 
-
-
 " Color Settings "
 # Functions: Preferences > Editor > Python > Functions
     # 88C0D0 (blue)
@@ -23,52 +21,32 @@
     # BF616A (pepto bismol)
     # 10A5BD (bright blue)
 
-
-
 " Color Theme "
 # Preferences > Appearance & Behavior > Appearance > Theme
-
-
 
 " Download Modules"
 #  View > Tool Windows > Python Packages
 
-
-
 " Download Packages "
 # Preferences > Project > Python Interpreter > Click 'Install' or Command + N
-
-
 
 " Keymap "
 # Preferences > Keymap
 
-
-
 " Multiple statements on one line "
 y = 3; x = 4; print(x+y)
-
-
 
 " Plugins " 
 # Preferences > Plugins
 
-
-
 " Preferences "
 # Command + ,
-
-
 
 " Run Code "
 # Run code in Python Console: Shift + Enter
 
-
-
 " Toggle Zen Mode " 
 # Fn + F8
-
-
 
 " Vertical Selection "
 # Command + Shift + 8
--- Visual Studio Code Notes.py
+++ Visual Studio Code Notes.py
@@ -5,53 +5,35 @@
 " Color Scheme"
 # Open Command Palette: type 'color' > select Color Theme
 
-
-
 " Command Palette "
 # Shift + Command + P
-
-
 
 " Comments "
 # # Comment
 # """ Block text """
 # Comment out text: highlight code or text > Command + /
 
-
-
 " Install Extensions "
 # Click Extensions icon (four squares) on Side Bar
 # Or 
 # Shift + Command + X
 
-
-
 " Install Packages "
 # Open Terminal > run "pip install package_name"
-
-
 
 " Interactive Window "
 # Open Interactive Window: Control + I
 # Run code in Interactive Window: Shift + Enter
 # Run code in Terminal: Control + Enter
 
-
-
 " Jupyter Notebook "
 # Open Command Palette: search for 'Create New Jupyter Notebook'
-
-
 
 " Keybindings "
 # Open Command Palette: search for 'JSON' > select keybindings.json > enter/edit keybindings
 
-
-
 " Macros "
 # Open Command Palette: search for 'JSON' > select settings.json > enter/edit macros
-
-
 
 " Python Version "
 import sys
@@ -59,23 +41,15 @@ print(sys.version)
 # 3.9.3 (v3.9.3:e723086bc3, Apr  2 2021, 08:25:55)
 print(sys.executable)
 
-
-
 " Run Code "
 # In Interactive Window: Shift + Enter
 # In Terminal: Ctrl + Enter
 
-
-
 " Search "
 # Command + P
 
-
-
 " Settings "
 # Command + ,
-
-
 
 " Terminal "
 # Ctrl + Shift + `
