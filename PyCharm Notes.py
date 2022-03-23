c298eab4adf922972d99d41f0639e54beb18c733
jefang15 <jeffreyfang@msn.com>
2022-03-22 21:59:47 -0400

    started code that simplifies scrape

--- PyCharm Notes.py
+++ PyCharm Notes.py
@@ -9,17 +9,21 @@
 
 " Color Settings "
 # Functions: Preferences > Editor > Python > Functions
-    # 88C0D0 (blue)
+#   88C0D0 (blue)
 # Text within triple and double quotes: Preferences > Editor > Color Scheme > Python > String > Text (unicode)
-    # A3BE8C (green)
-    # D7BA7D (mustard)
+#   A3BE8C (green)
+#   D7BA7D (mustard)
 # Numbers: Preferences > Editor > Color Scheme > Python > Number
-    # B48EAD (purple)
-    # EF9E4F (orange)
-    # F1969F (pink)
+#   B48EAD (purple)
+#   EF9E4F (orange)
+#   F1969F (pink)
 # Highlight: Preferences > Editor > General > Selection Background
-    # BF616A (pepto bismol)
-    # 10A5BD (bright blue)
+#   BF616A (pepto bismol)
+#   10A5BD (bright blue)
+# Comments: Preferences > Editor > Color Scheme > Language Defaults > Line Comment
+#   616E88 (grey)
+#   B0BBD0 (brighter grey/change to this)
+
 
 " Color Theme "
 # Preferences > Appearance & Behavior > Appearance > Theme
