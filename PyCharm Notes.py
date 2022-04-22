d8821b6a5cfa88e925afc8248c355ef6bab2e033
jefang15 <jeffreyfang@msn.com>
2022-04-21 22:41:59 -0400

    Update PyCharm color assignments

--- PyCharm Notes.py
+++ PyCharm Notes.py
@@ -7,22 +7,43 @@
 # """ Block text """
 # Comment out text: highlight code or text > Command + /
 
+
 " Color Settings "
+# Text: Preferences > Editor > Color Scheme > General > Text > Default Text
+#   D8DEE9 (default off white)
+# Comments: Preferences > Editor > Color Scheme > Language Defaults > Comments > Line Comment
+#   616E88 (default grey)
+#   758198 (lighter than default grey)
+#   D8DEE9 (default off white)
+# Comments: Preferences > Editor > Color Scheme > Python > Line Comment
+#   616E88 (default grey)
+#   D8DEE9 (default off white)
 # Functions: Preferences > Editor > Python > Functions
 #   88C0D0 (blue)
 # Text within triple and double quotes: Preferences > Editor > Color Scheme > Python > String > Text (unicode)
 #   A3BE8C (green)
 #   D7BA7D (mustard)
 # Numbers: Preferences > Editor > Color Scheme > Python > Number
-#   B48EAD (purple)
 #   EF9E4F (orange)
 #   F1969F (pink)
+#   A3BE8C (green)
+#   B48EAD (purple)
 # Highlight: Preferences > Editor > General > Selection Background
 #   BF616A (pepto bismol)
 #   10A5BD (bright blue)
-# Comments: Preferences > Editor > Color Scheme > Language Defaults > Line Comment
-#   616E88 (grey)
-#   B0BBD0 (brighter grey/change to this)
+# Background: Preferences > Editor > Color Scheme > General > Text > Default Text > Background
+#   2E3440 (grey)
+# Keyword: Preferences > Editor > Color Scheme > Language Defaults > Keyword
+#   81A1C1 (default blue)
+#   B48EAD (purple)
+# Keyword: Preferences > Editor > Color Scheme > Python > Keyword
+#   81A1C1 (default blue)
+#   B48EAD (purple)
+# Built-in Name: Preferences > Editor > Color Scheme > Python > Braces and Operators > Built-in Name
+#   81A1C1 (default blue)
+#   FFC1F3 (pink/purple)
+
+
 
 " Color Theme "
 # Preferences > Appearance & Behavior > Appearance > Theme
